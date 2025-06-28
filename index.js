360283620660af34b01c994401cc4bde06558ad3
scherepi <scherejoaquin@gmail.com>
2025-06-28 16:02:54 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+/*
+Reverend X Quotes:
+
+"Murder, a devil worshipper will do. That's John chapter 8 verse 42. The Devil does not abide by the truth, because there is no truth in him."
+
+"I don't give a fuck what you think, bitch. Your thoughts ain't my thoughts. Bitch, I'm flowing straight from the survival scroll."
+
+"CUT THAT BITCH OFF! Next caller."
+
+"What's happening?"
+
+"You don't know 'em?"
+
+"You're goddamn straight."
+
+"Debbie? Is that Debbie? Do you understand? Do you understand what I'm doing? Is you down with that? I'm serious about business. You know it's business before pleasure."
+
+"This is the knowledge and the will of almighty god."
+
+"It's my world. That's including your asshole too."
+
+"Fuck what you talking 'bout."
+
+"I am God."
+
+"I come in the name of Jesus by the power of the holy spirit."
+
+"You don't know my words, your best bet is to learn. Now who the fuck is you supposed to be? Don't act like you don't know me, fool. What? You don't? Ain't no shame on me motherfucker, shame on you."
+
+"I am the light. And you, motherfucker, you just blind."
+
+"Well, you've got to be out yo goddamn mind."
+
+"If you don't mind, god don't mind slaying yo wicked ass at the end of time." */
+
+const imageFilenames = ["idle.png", "dance.png"]
+
+const revXQuotes = [
+    "Murder, a devil worshipper will do. That's John chapter 8 verse 42. The Devil does not abide by the truth, because there is no truth in him.", 
+    "I don't give a fuck what you think, bitch. Your thoughts ain't my thoughts. Bitch, I'm flowing straight from the survival scroll.", 
+    "CUT THAT BITCH OFF! Next caller.",
+    "What's happening?",
+    "You don't know 'em?",
+    "You're goddamn straight.",
+    "Debbie? Is that Debbie? Do you understand? Do you understand what I'm doing? Is you down with that? I'm serious about business. You know it's business before pleasure.",
+    "This is the knowledge and the will of almighty god.",
+    "It's my world. That's including your asshole too.",
+    "Fuck what you talking 'bout.",
+    "I am God.",
+    "I come in the name of Jesus by the power of the holy spirit.",
+    "You don't know my words, your best bet is to learn. Now who the fuck is you supposed to be? Don't act like you don't know me, fool. What? You don't? Ain't no shame on me motherfucker, shame on you.",
+    "I am the light. And you, motherfucker, you just blind.",
+    "Well, you've got to be out yo goddamn mind.",
+    "If you don't mind, god don't mind slaying yo wicked ass at the end of time."
+]
+
+const rev = document.getElementById("rev");
+const gospel = document.getElementById("gospel");
+console.log("Starting script - Reverend X loves you, motherfucker.");
+
+function rotate() {
+    
+}
